9c05d6091e60d8adc4ef533fdf25c3ec6f850dc5
ING <asnazal@miuandes.cl>
2018-04-03 16:13:16 -0300

    First commit for april 3 lab

---
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Vehiculo
+    {
+        String Nombre;
+        String Marca;
+        String Modelo;
+        String Tipo;
+        String Patente;
+        int Año;
+        int Kilometros;
+        int Precio;
+    }
+    CrearVehiculo(Vehiculo)
+    {
+        Vehiculo v1 = new Vehiculo(Console.WriteLine("Nombre del Vehículo a ingresar"), Console.WriteLine("Marca del Vehículo a ingresar"),
+            Console.WriteLine("Modelo del Vehículo a ingresar"), Console.WriteLine("Tipo de Vehículo a ingresar (Moto, auto, 4x4, etc)"),
+            Console.WriteLine("Patente del Vehículo a ingresar"), Console.WriteLine("Año de fabricación del Vehículo a ingresar"),
+            Console.WriteLine("Kilometraje del Vehículo a ingresar"), Console.WriteLine("Precio diario de arriendo del Vehículo a ingresar"));
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Class8.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Arriendo
+    {
+        int numero;
+
+    }
+    DatosArriendo(Persona, Empresa, Vehiculo, Accesorio, Sucursal)
+    {
+        Persona p1 = new Persona();
+        Empresa e1 = new Empresa();
+        Vehiculo v1 = new Vehiculo();
+        Accesorio a1 = new Accesorio();
+        Sucursal s1 = new Sucursal();
+    }
+
+    Arrendar(Vehiculo)
+    {
+        Console.WriteLine("Hola, bienvenido a Arrienda Tu Auto. Ingresa Los Datos Personales Pedidos.");
+        Console.WriteLine("Datos Ingresados Correctamente! Elige tu vehículo");
+        Console.WriteLine("Vehículo Seleccionado Exitosamente! Elige los Accesorios que desees");
+    }
+
+    AgregarAccesorios(Accesorio)
+    {
+        Console.WriteLine("Accesorios Seleccionados Exitosamente! El total es de" + PrecioTotal + "Elige medio de pago");
+        Console.WriteLine("Pago realizado con Exito! Resumen de su arriendo:" + DatosArriendo);
+    }
+
+     PrecioTotal(Vehiculo, Accesorio)
+    {
+        Arrendar.get(Precio) + AgregarAccesorios.get(Precio); 
+    }
+}
